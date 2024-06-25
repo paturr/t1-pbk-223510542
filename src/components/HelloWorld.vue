9cf1a7e1f1538d309fa52cf996f934fd47906318
paturr <fathurazrahman016@gmail.com>
2024-06-25 21:41:46 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+    Hallo Saya<br> Fathur Azrahman, Saya memiliki banyak hobi, dan salah satu hobi saya adalah bermain game.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+  color: blueviolet
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,52 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>PENDIDIKAN</template>
+    PENDIDIKAN YANG DITEMPUH<br> SD (004 Salo)<br> SMP (SMP-IT Al- Utsaimin Ridan Permai, Bangkinang)<br> SMA (SMAN 1 Bangkinang Kota)
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>OLahraga</template>
+    Ketertarikan dengan dunia Olahraga<br> Sepakbola
+    <br />
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>GAMES</template>
+    Games Favorite untuk saat ini,<br> 1.eFootbal 2024<br> 2.Ball 8 Pool<br> 3.PUBG
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>MEDIA SOSIAL</template>
+    -Instagram : @fathur.azrahman<br>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>DATA DIRI</template>
+    1.NAMA : Fathur Azrahman<br> 2.UMUR : 20<br> 3.MAHASISWA : Universitas Islam Riau<br> 4.JENIS KELAMIN : LAKI-LAKI
+    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener"></a>
+  </WelcomeItem>
+</template>
